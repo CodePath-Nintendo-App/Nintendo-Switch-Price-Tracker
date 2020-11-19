ccca67c353d509dde5f7e9cbacd19188ff61414d
Konstantin Novichenko <novichenko.konstantin@gmail.com>
2020-11-19 15:40:36 -0500

    Initial Commit

---
+++ GoogleOAuthTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleOAuthTest
+//
+//  Created by Konstantin Novichenko on 11/19/20.
+//  Copyright © 2020 konst_nvc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
