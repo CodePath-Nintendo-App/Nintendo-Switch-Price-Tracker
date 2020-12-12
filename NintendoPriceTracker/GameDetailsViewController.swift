7ad7cff71019d4fa751bd8314be93e5474beac35
Melchizedek Tetteh <Tettehmel@gmail.com>
2020-12-11 22:42:38 -0500

    youtube video implemented completely

--- NintendoPriceTracker/GameDetailsViewController.swift
+++ NintendoPriceTracker/GameDetailsViewController.swift
@@ -9,8 +9,11 @@ import UIKit
 import AlamofireImage
 
 class GameDetailsViewController: UIViewController {
-
+    
+    
     var games = [Game]()
+    var ytDetails = [[String:Any]]()
+    var videoId = String()
     
     @IBOutlet var playerView: YTPlayerView!
     
@@ -25,11 +28,28 @@ class GameDetailsViewController: UIViewController {
         self.gameTitleLabel.sizeToFit()
         self.bannerImageView.af.setImage(withURL: URL(string: games[0].imageUrlString)!)
         
-        let url = URL(string: "https://www.googleapis.com/youtube/v3/search?channelId=UCKy1dAqELo0zrOtPkf0eTMw")!
+        let url = URL(string: "https://www.googleapis.com/youtube/v3/search?channelId=UCKy1dAqELo0zrOtPkf0eTMw&q=cyberpunk&key=AIzaSyChGOHUTWczNJ6TqxnZvrZffKFczMS9-58")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+                    // This will run when the network request returns
+                    if let error = error {
+                        print(error.localizedDescription)
+                    } else if let data = data {
+                        let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                        print("test-------------------------------------")
+//                        print(dataDictionary["items"] as! [[String:Any]] )
+                        let videoDetails = dataDictionary["items"] as! [[String:Any]]
+                        let vidObj = videoDetails[0]["id"] as! [String:Any]
+                        let videoId = vidObj["videoId"] as! String
+                        self.playerView.load(withVideoId: videoId, playerVars:["playsinline":1])
+                    }
+                }
+                task.resume()
         
         
-        playerView.load(withVideoId: "ur6I5m2nTvk", playerVars:["playsinline":1])
+//        playerView.load(withVideoId: "j0mg7GEIpio", playerVars:["playsinline":1])
         
         // Do any additional setup after loading the view.
     }
@@ -44,5 +64,6 @@ class GameDetailsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
 
 }
